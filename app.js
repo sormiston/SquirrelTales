a80dc849a64e7ed80c61df82aed9c72af8a92a1d
SOrmiston <sean.david.ormiston@gmail.com>
2020-07-15 00:14:10 -0400

    Render Map now adds id-tagged coordinate grid of divs within mapView

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ const hectareDisplay = document.querySelector('#hectare')
 const dataCall = document.querySelector('#fetch')
 const mapView = document.querySelector('#map-view')
 
-// local globals established by data fetch
+// local global variables
+const aYAxisLetters = ['A','B','C','D','E','F','G','H','I']
 let aLocalData = []
 let oSelected = {}
 let sHectare = undefined
+
 
 dataCall.addEventListener('click', getData)
 
@@ -59,7 +61,7 @@ function renderStory() {
 }
 
 function fadeText() {
-  console.log('fade text called')
+  // Can some of this functionality be moved to CSS under transition-delay property??              << SIMPLIFY (?)
   let i = 0
   let time = setInterval(() => {
     console.log(`Story Area timed render: ${storyArea.children[i]} at index ${i} of length ${storyArea.children.length}`)
@@ -71,10 +73,10 @@ function fadeText() {
     }
   }, 700)
 }
+
 function clearStory() {
-  while (storyArea.lastChild) {
-    storyArea.removeChild(storyArea.lastChild)
-  }
+  while (storyArea.lastChild) storyArea.removeChild(storyArea.lastChild)
+  
 }
 
 function renderIconBar() {
@@ -141,16 +143,38 @@ function renderIconBar() {
 }
   
 function renderMap() {
+  // clear previous grid
+  while (mapView.lastChild) mapView.removeChild(mapView.lastChild)
+  // Hard-coded to select between 2 map halves, each 21 x 9
   let sHectareX = sHectare.substr(0, 2)
   let sHectareY = sHectare[2]
   switch (sHectareX <= 21) {
-    case true:
+    case true: {
       if (!mapView.classList.contains('parkSouth')) mapView.classList.add('parkSouth')
       if (mapView.classList.contains('parkNorth')) mapView.classList.remove('parkNorth')
+      for (let i = 0; i <= 8; i++) {
+        for (let j = 1; j <= 21; j++) {
+          let newDiv = document.createElement('div')
+          newDiv.id = `${(j.toString()).padStart(2, '0')}${aYAxisLetters[i]}`
+          newDiv.classList.add('hectare')
+          console.log(newDiv.id)
+          mapView.append(newDiv)
+        }
+      }
       break
+    }
     case false:
       if (!mapView.classList.contains('parkNorth')) mapView.classList.add('parkNorth')
       if (mapView.classList.contains('parkSouth')) mapView.classList.remove('parkSouth')
+      for (let i = 0; i <= 8; i++) {
+        for (let j = 22; j <= 42; j++) {
+          let newDiv = document.createElement('div')
+          newDiv.id = `${(j.toString()).padStart(2, '0')}${aYAxisLetters[i]}`
+          newDiv.classList.add('hectare')
+          console.log(newDiv.id)
+          mapView.append(newDiv)
+        }
+      }
       break
     default:
       console.error('Hectare parse error')
